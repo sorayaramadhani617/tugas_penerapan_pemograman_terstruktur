041483a4958f4c03784e9afc2f5cdfe71155e1f4
sorayaramadhani617 <32492657+sorayaramadhani617@users.noreply.github.com>
2018-01-03 16:51:57 +0700

    Menampilkan Nilai Matriks X Dengan Perulangan

--- Soraya_Ramadhani.cpp
+++ Soraya_Ramadhani.cpp
@@ -29,6 +29,23 @@ int main (){
 	double ha [ 3 ] = { RWa1c [ 0 ] + RWa1c [ 1 ] + RWa1c [ 2 ] + RWa1c [ 3 ] + RWa1c [ 4 ],
 					    RWa2c [ 0 ] + RWa2c [ 1 ] + RWa2c [ 2 ] + RWa2c [ 3 ] + RWa2c [ 4 ],
 				        RWa3c [ 0 ] + RWa3c [ 1 ] + RWa3c [ 2 ] + RWa3c [ 3 ] + RWa3c [ 4 ]};
+				      
+	cout << " MATRIKS X : "<<endl;
+	
+	int a=0;
+	do{
+		cout << a1c [ a ] << " | "; a++;}
+		while ( a<5 ); cout<<endl;
+	
+	int b=0;
+	do{
+		cout << a2c [ b ] << " | "; b++;}
+		while ( b<5 ); cout<<endl;
+		
+	int c=0;
+	do{
+		cout << a3c [ c ] << " | "; c++;}
+		while ( c<5 ); cout<<endl;
 
 }
 		
