e1d77f19fc03f3fa71eb02def135f0ff3226beca
sorayaramadhani617 <32492657+sorayaramadhani617@users.noreply.github.com>
2018-01-03 16:59:27 +0700

    Menampilkan Nilai Akhir Menggunakan Array 1 Dimensi

--- Soraya_Ramadhani.cpp
+++ Soraya_Ramadhani.cpp
@@ -89,6 +89,13 @@ cout <<endl << endl;
 			cout <<RWa3c[r] << " | ";
 			r++;
 		}while(r<5);cout << endl;
+		
+		cout << endl << endl;
+	cout << endl << endl;
+	cout << " Hasil Akhir : " <<endl ;
+	cout << " Nilai A1 : " << ha[ 0 ] <<endl;
+	cout << " Nilai A2 : " << ha[ 1 ] <<endl;
+	cout << " Nilai A3 : " << ha[ 2 ] <<endl;
 }
 		
 	
